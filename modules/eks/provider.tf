9f1759b757ad07fcc403b74ad473b05626aa5549
Olena <lenafurmaniuk@gmail.com>
2024-12-05 17:12:23 -0600

    provider update

--- modules/eks/provider.tf
+++ modules/eks/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 0.14"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0"
+      version = ">= 0.14"
     }
   }
 }
