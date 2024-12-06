6a852f9225bc41f1920da5c3fe1ed52e7f9df2f8
Olena <lenafurmaniuk@gmail.com>
2024-12-05 18:20:21 -0600

    u

--- modules/eks/provider.tf
+++ modules/eks/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 0.14"
+      version = "~> 5.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = ">= 0.14"
+      version = "~> 4.0"
     }
   }
 }
---
+++ versions.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.0.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+  
+  backend "s3" {
+    bucket  = "ofurmaniuk"
+    key     = "terraform-project/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
