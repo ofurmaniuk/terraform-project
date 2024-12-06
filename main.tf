d43f616777efdc21776ef39caf815ab25b7cc7c5
Olena <lenafurmaniuk@gmail.com>
2024-12-06 13:15:02 -0600

    update

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# VPC Module
 module "vpc" {
   source = "./modules/vpc"
 
@@ -10,24 +9,20 @@ module "vpc" {
   environment     = var.environment
   az_a           = var.az_a
   az_b           = var.az_b
-  
 }
 
-# RDS Module
 module "rds" {
   source = "./modules/rds"
 
-  vpc_id                         = module.vpc.vpc_id
-  private_subnet_ids             = values(module.vpc.private_subnets)
-  web_security_group_id          = module.vpc.web_security_group_id
-  eks_cluster_security_group_id  = module.eks.cluster_security_group_id
-  environment                    = var.environment
-  db_name                        = var.db_name
-  master_username                = var.master_username
-  
+  vpc_id                        = module.vpc.vpc_id
+  private_subnet_ids           = values(module.vpc.private_subnets)
+  web_security_group_id        = module.vpc.web_security_group_id
+  eks_cluster_security_group_id = module.eks.cluster_security_group_id
+  environment                  = var.environment
+  db_name                      = var.db_name
+  master_username              = var.master_username
 }
 
-# EKS Module
 module "eks" {
   source = "./modules/eks"
 
@@ -35,5 +30,4 @@ module "eks" {
   vpc_id            = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnets
   public_subnet_ids  = module.vpc.public_subnets
-  
 }
