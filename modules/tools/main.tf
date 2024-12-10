2fdf7cf36d5133db6eb90a66995d5f4e65434f8c
Olena <lenafurmaniuk@gmail.com>
2024-12-10 17:55:05 -0600

    u

--- modules/tools/main.tf
+++ modules/tools/main.tf
@@ -1,45 +1,45 @@
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    name = "argocd"
-    labels = {
-      name = "argocd"
-    }
-  }
-}
+# resource "kubernetes_namespace" "argocd" {
+#   metadata {
+#     name = "argocd"
+#     labels = {
+#       name = "argocd"
+#     }
+#   }
+# }
 
-resource "kubernetes_namespace" "monitoring" {
-  metadata {
-    name = "monitoring"
-    labels = {
-      name = "monitoring"
-    }
-  }
-}
+# resource "kubernetes_namespace" "monitoring" {
+#   metadata {
+#     name = "monitoring"
+#     labels = {
+#       name = "monitoring"
+#     }
+#   }
+# }
 
-resource "helm_release" "argocd" {
-  name       = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  version    = "5.46.7"
-  namespace  = kubernetes_namespace.argocd.metadata[0].name
+# resource "helm_release" "argocd" {
+#   name       = "argocd"
+#   repository = "https://argoproj.github.io/argo-helm"
+#   chart      = "argo-cd"
+#   version    = "5.46.7"
+#   namespace  = kubernetes_namespace.argocd.metadata[0].name
 
-  values = [
-    file("${path.module}/values/argocd-values.yaml")
-  ]
+#   values = [
+#     file("${path.module}/values/argocd-values.yaml")
+#   ]
 
-  depends_on = [kubernetes_namespace.argocd]
-}
+#   depends_on = [kubernetes_namespace.argocd]
+# }
 
-resource "helm_release" "prometheus" {
-  name       = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  version    = "51.9.3"
-  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+# resource "helm_release" "prometheus" {
+#   name       = "prometheus"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "kube-prometheus-stack"
+#   version    = "51.9.3"
+#   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
-  values = [
-    file("${path.module}/values/prometheus-values.yaml")
-  ]
+#   values = [
+#     file("${path.module}/values/prometheus-values.yaml")
+#   ]
 
-  depends_on = [kubernetes_namespace.monitoring]
-}
+#   depends_on = [kubernetes_namespace.monitoring]
+# }
