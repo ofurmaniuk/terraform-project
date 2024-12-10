6f3db4b19ccbce3e3e661ffbdab1ca3624ecad4c
Olena <lenafurmaniuk@gmail.com>
2024-12-10 17:49:12 -0600

    files added

---
+++ modules/tools/variables.tf
@@ -0,0 +1,14 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "cluster_endpoint" {
+  description = "Kubernetes cluster endpoint"
+  type        = string
+}
+
+variable "cluster_ca_certificate" {
+  description = "Kubernetes cluster CA certificate"
+  type        = string
+}
