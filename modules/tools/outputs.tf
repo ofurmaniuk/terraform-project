c00e70a66690aef8cde756988898326ea0864b2b
Olena <lenafurmaniuk@gmail.com>
2024-12-17 15:09:55 -0600

    added  values files

--- modules/tools/outputs.tf
+++ modules/tools/outputs.tf
@@ -8,7 +8,3 @@ output "prometheus_url" {
   value       = "http://${helm_release.prometheus.name}-server"
 }
 
-output "cluster_certificate_authority_data" {
-  description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = aws_eks_cluster.main.certificate_authority[0].data
-}
