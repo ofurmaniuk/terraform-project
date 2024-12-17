df637089a9ef40821650ec571ab17e2bbe5728ba
Olena <lenafurmaniuk@gmail.com>
2024-12-17 14:55:02 -0600

    eks module added certificate

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -17,3 +17,8 @@ output "node_group_id" {
   description = "EKS Node Group ID"
   value       = aws_eks_node_group.main.id
 }
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
