df3f3916e2c6ea472ff926955b9f1dd7741c595b
Olena <lenafurmaniuk@gmail.com>
2024-12-17 14:35:21 -0600

    added tools files

--- modules/tools/outputs.tf
+++ modules/tools/outputs.tf
@@ -1,9 +1,9 @@
-# output "argocd_url" {
-#   description = "ArgoCD Server URL"
-#   value       = "https://${helm_release.argocd.name}-server"
-# }
+output "argocd_url" {
+  description = "ArgoCD Server URL"
+  value       = "https://${helm_release.argocd.name}-server"
+}
 
-# output "prometheus_url" {
-#   description = "Prometheus Server URL"
-#   value       = "http://${helm_release.prometheus.name}-server"
-# }
+output "prometheus_url" {
+  description = "Prometheus Server URL"
+  value       = "http://${helm_release.prometheus.name}-server"
+}
