620886fbd8f84355fc85544e07c7953bb530a8fb
Olena <lenafurmaniuk@gmail.com>
2024-12-19 12:16:44 -0600

    versions update

--- modules/tools/versions.tf
+++ modules/tools/versions.tf
@@ -8,5 +8,9 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14.0"
+    }
   }
 }
