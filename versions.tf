9443857505752905f7680d3214abdb288c3f4881
Olena <lenafurmaniuk@gmail.com>
2024-12-19 16:23:11 -0600

    update

--- versions.tf
+++ versions.tf
@@ -14,6 +14,11 @@ terraform {
       source  = "hashicorp/tls"
       version = "~> 4.0"
     }
+
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14.0"
+    }
   }
 }
 
