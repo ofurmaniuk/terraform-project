905fdc65821ea4d5f11fb0849d3a013df0ba35de
Olena <lenafurmaniuk@gmail.com>
2025-01-08 14:53:32 -0600

    file update

--- modules/tools/main.tf
+++ modules/tools/main.tf
@@ -16,22 +16,3 @@ resource "kubernetes_namespace" "monitoring" {
   }
 }
 
-resource "helm_release" "argocd" {
-  name       = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  version    = "5.46.7"
-  namespace  = kubernetes_namespace.argocd.metadata[0].name
-
-  
-# values = [
-#     file("${path.module}/values/argocd-values.yaml")
-#   ]
-  depends_on = [
-    helm_release.ingress_nginx,
-    kubernetes_namespace.argocd
-  ]
-  wait         = true
-  wait_for_jobs = true
-  timeout      = 600
-}
