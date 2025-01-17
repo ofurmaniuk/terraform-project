cf0bbf3b11ba08001fb3869373f8c3e94c4365af
Olena <lenafurmaniuk@gmail.com>
2025-01-17 15:11:18 -0600

    update

--- modules/tools/versions.tf
+++ modules/tools/versions.tf
@@ -3,14 +3,17 @@ terraform {
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.11.0"
+      configuration_aliases = [helm]
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23.0"
+      configuration_aliases = [kubernetes]
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = "~> 1.14.0"
+      configuration_aliases = [kubectl]
     }
     time = {
       source  = "hashicorp/time"
